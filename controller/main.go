68eae02d76931ffdeeacd137135fe3c1dbe4c740
zkynet <anon@zkynet.io>
2019-09-01 09:00:07 +0300

    refactored some logging and removed some code.

--- controller/main.go
+++ controller/main.go
@@ -14,7 +14,6 @@ import (
 	"runtime/debug"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/zkynetio/lynx/alerting"
@@ -27,6 +26,26 @@ import (
 var GlobalController *Controller
 var GlobalBrain *Brain
 
+type Controller struct {
+	safelocker.SafeLocker
+	// Recovery in sqlite?
+	//RecoveryFile string
+	Config         ControllerConfig
+	PORT           string
+	IP             string
+	Collectors     map[string]*Collector
+	UIs            map[string]*ui.UI
+	UIServer       *ui.UIServer
+	UIParseChannel chan DPCollection
+	UISendChannel  chan []byte
+}
+type Collector struct {
+	TAG         string
+	Conn        net.Conn
+	LastCheckin time.Time
+	SendChannel chan string
+	Namespaces  map[int]string
+}
 type Brain struct {
 	safelocker.SafeLocker
 	Socket      net.Conn
@@ -60,6 +79,33 @@ type ControllerConfig struct {
 	}
 }
 
+func (b *Brain) connectToBrain(address string) (config ControllerConfig, err error) {
+
+	var socket net.Conn
+	socket, err = net.Dial("tcp", b.Address)
+	if err != nil {
+		return
+	}
+
+	lengthBytes := make([]byte, 2)
+	_, err = socket.Read(lengthBytes)
+	if err != nil {
+		return
+
+	}
+	data := make([]byte, binary.LittleEndian.Uint16(lengthBytes))
+	_, err = socket.Read(data)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return
+	}
+	b.Socket = socket
+	return
+}
 func Start(address string) {
 	rand.Seed(time.Now().Unix())
 	namespaces.Init()
@@ -67,45 +113,40 @@ func Start(address string) {
 		Address: address,
 	}
 	GlobalBrain = &Brain
-	socket, err := net.Dial("tcp", Brain.Address)
-	if err != nil {
-		panic(err)
-	}
-
-	lengthBytes := make([]byte, 2)
-	_, err = socket.Read(lengthBytes)
-	if err != nil {
-		log.Println("Error reading length bytes from brain", err)
-		os.Exit(1)
-	}
-	data := make([]byte, binary.LittleEndian.Uint16(lengthBytes))
-	_, err = socket.Read(data)
-	if err != nil {
-		log.Println("Error reading data fom brain ...", err)
-		os.Exit(1)
-	}
-
-	// log.Println(string(data))
 	var config ControllerConfig
-	err = json.Unmarshal(data, &config)
-	if err != nil {
-		log.Println("Something went wrong when receiving a config from the brain", err)
-		os.Exit(1)
+	var err error
+	for {
+		time.Sleep(2 * time.Second)
+		config, err = Brain.connectToBrain(address)
+		if err != nil {
+			log.Println("Error while reaching out to the brain:", err)
+			continue
+		}
+		break
 	}
-	log.Println("GOT A CONFIG:", config)
-	// os.Exit(1)
-	Brain.Socket = socket
+	log.Println("/////////////////////////////////////////////////////")
+	log.Println("I managed to reach the brain @", address)
+	log.Println("Controller default IP:", config.IP)
+	log.Println("Collector IP/PORT:", config.Collector)
+	log.Println("UI IP/PORT:", config.UI)
+	log.Println("Debug:", config.Debug)
+	log.Println("/////////////////////////////////////////////////////")
 	Brain.SendChannel = make(chan []byte, 10000)
 
 	UIServer := ui.NewUIServer()
 	UIServer.ClientList = make(map[string]*ui.UI)
-	UIServer.IP = config.UI.IP
+
+	if config.UI.IP == "" {
+		UIServer.IP = config.IP
+	} else {
+		UIServer.IP = config.UI.IP
+	}
+
 	UIServer.Port = strconv.Itoa(config.UI.Port)
 
 	controller := Controller{
 		Config:     config,
 		Collectors: make(map[string]*Collector),
-		Buffer:     make(chan *DataPoint, 1000000),
 		UIServer:   UIServer,
 	}
 	// os.Exit(1)
@@ -119,14 +160,7 @@ func Start(address string) {
 
 	GlobalController = &controller
 
-	LiveBuffer = &Collection{
-		Map:               make(map[string]map[string]map[uint64][]byte),
-		CurrentBase:       make(map[string]map[string]int64),
-		CollectorStatsMap: make(map[string]map[string]int64),
-	}
 	defer controller.CleanupOnExit()
-	go controller.EngageBufferPipe()
-
 	go UIServer.Start(watcherChannel)
 	go controller.start(watcherChannel, closeChannel)
 	stop := make(chan os.Signal, 1)
@@ -144,11 +178,12 @@ func Start(address string) {
 				go Brain.MaintainLinkToBrain(watcherChannel, closeChannel)
 			}
 
-			log.Println("goroutine number", index, "just closed...")
+			log.Println("Goroutine watcher detected a crash in goroutine number:", index)
 			break
-		case <-stop:
-			// TODO: handle exit gracefully
-			log.Println("handle exit gracefully")
+		case signal := <-stop:
+			log.Println("Signal received (", signal, ") controller will now exit gracefully")
+			controller.CleanupOnExit()
+
 			os.Exit(1)
 		}
 	}
@@ -227,8 +262,7 @@ func (b *Brain) DecodeConfig(closeChannel chan bool, data []byte) error {
 		log.Println(err)
 		return err
 	} else if config.IP == "" {
-		log.Println("IP missing, Assuming an errror")
-		return errors.New("IP missing, Assuming an errror")
+		return errors.New("Not updating config, IP missing")
 	} else if config.Shutdown {
 		log.Println("Brain told me to exit...")
 		os.Exit(1)
@@ -258,48 +292,15 @@ func (b *Brain) DecodeConfig(closeChannel chan bool, data []byte) error {
 
 }
 
-type Controller struct {
-	safelocker.SafeLocker
-	Buffer chan *DataPoint
-	// Recovery in sqlite?
-	//RecoveryFile string
-	Config         ControllerConfig
-	PORT           string
-	IP             string
-	Collectors     map[string]*Collector
-	UIs            map[string]*ui.UI
-	UIServer       *ui.UIServer
-	UIParseChannel chan DPCollection
-	UISendChannel  chan []byte
-}
-type Collection struct {
-	// instace,namespace,[year.month.day.hour.minute.second],[]byte
-	Map               map[string]map[string]map[uint64][]byte
-	CurrentBase       map[string]map[string]int64
-	CollectorStatsMap map[string]map[string]int64
-	Mux               sync.Mutex
-}
-
-var LiveBuffer *Collection
-
 // mat zoe? abive and beyond
 func (c *Controller) CleanupOnExit() {
-	helpers.DebugLog("Cleaning up on exit...")
+	helpers.DebugLog("Post exit cleanup ..")
 	for _, collector := range c.Collectors {
 		if collector.Conn != nil {
-			_, _ = collector.Conn.Write([]byte("c\n"))
-			helpers.DebugLog("Closing:", collector.TAG)
+			helpers.DebugLog("Closing connection to:", collector.TAG, "@", collector.Conn.RemoteAddr().String())
 			_ = collector.Conn.Close()
 		}
 	}
-}
-
-type Collector struct {
-	TAG         string
-	Conn        net.Conn
-	LastCheckin time.Time
-	SendChannel chan string
-	Namespaces  map[int]string
 }
 
 func (c *Controller) AddCollector(TAG string, collector *Collector) error {
@@ -327,7 +328,16 @@ func (controller *Controller) start(watcherChannel chan int, closeChannel chan b
 		watcherChannel <- 2
 	}(watcherChannel)
 	helpers.DebugLog("listening on:", controller.Config.Collector.IP+":"+strconv.Itoa(controller.Config.Collector.Port))
-	ln, err := net.Listen("tcp", controller.Config.Collector.IP+":"+strconv.Itoa(controller.Config.Collector.Port))
+
+	address := ""
+	if controller.Config.Collector.IP == "" {
+		address = address + controller.Config.IP + ":" + strconv.Itoa(controller.Config.Collector.Port)
+	} else {
+		address = address + controller.Config.Collector.IP + strconv.Itoa(controller.Config.Collector.Port)
+	}
+
+	ln, err := net.Listen("tcp", address)
+
 	helpers.PanicX(err)
 	for {
 		conn, err := ln.Accept()
@@ -448,13 +458,6 @@ func readFromConnectionOriginal(collector *Collector, controller *Controller) {
 
 }
 
-type DataPoint struct {
-	Value       []byte
-	Tag         string
-	Timestamp   int64
-	ControlByte int
-}
-
 func (c *Controller) HandleDataPoint(collector *Collector, data []byte, controlByte int) {
 	defer func() {
 		// recover if we get a broken data point.
@@ -483,96 +486,3 @@ func (c *Controller) HandleDataPoint(collector *Collector, data []byte, controlB
 
 	c.UIParseChannel <- DPC
 }
-
-func (c *Controller) saveData(tag string, data []byte, controlByte int) {
-	c.Buffer <- &DataPoint{
-		Value:       data,
-		Tag:         tag,
-		ControlByte: controlByte,
-	}
-}
-func (c *Controller) EngageBufferPipe() {
-
-	// TODO: grow properly, we are writing strings.. but the buffer grows in bytes
-	//	buffer.Grow(c.MinLinesInBufferFile * 500)
-	// size := 0
-	for {
-		if len(c.Buffer) > 100 {
-			//buffer.Grow(buffer.Len() * 2)
-			helpers.DebugLog("chan length", len(c.Buffer))
-		}
-
-		// CollectionBuffer.mux.Lock()
-		// c.CollectionBuffer[dp.Tag] = append(c.CollectionBuffer[dp.Tag], dp)
-		// CollectionBuffer.mux.Unlock()
-		c.ParseDataPointIntoMemoryMap(<-c.Buffer)
-		// log.Println(c.CollectionBuffer)
-		// size = size + len(dp.Value)
-		// if size > 3000 {
-		// 	// write data
-		// 	// go c.WriteBufferToFile()
-		// }
-
-	}
-}
-func (c *Controller) ParseDataPointIntoMemoryMap(dp *DataPoint) {
-
-	// timestamp := dp.Timestamp.Unix()
-	// tag := dp.Tag
-	// log.Println(dp.Value)
-	timestamp := binary.LittleEndian.Uint64(dp.Value[:8])
-	// log.Println("data starting sequence:", dp.Value[8:20])
-	// log.Println("timestamp", timestamp, dp.Value[:8])
-
-	// tm := time.Unix(0, int64(timestamp))
-	// fmt.Println(tm.Format(time.RFC3339))
-	//
-	// log.Println("Data from:", dp.Tag)
-	// log.Println(dp.Value[8:])
-	// ParseDataPoint(dp.Value[8:])
-	// for _, v := range dp.Value {
-	if LiveBuffer.Map[dp.Tag] == nil {
-		LiveBuffer.Map[dp.Tag] = make(map[string]map[uint64][]byte)
-	}
-	if LiveBuffer.Map[dp.Tag][dp.Tag] == nil {
-		LiveBuffer.Map[dp.Tag][dp.Tag] = make(map[uint64][]byte)
-	}
-
-	LiveBuffer.Mux.Lock()
-	LiveBuffer.Map[dp.Tag][dp.Tag][timestamp] = dp.Value
-	LiveBuffer.Mux.Unlock()
-	//
-	// log.Println(LiveBuffer.Map)
-	// for i, v := range LiveBuffer.Map {
-	// 	for _, iv := range v {
-	// 		for iii, _ := range iv {
-	// 			log.Println("A Record:", i, iii)
-	// 		}
-	// 	}
-	// }
-}
-
-// func (c *Controller) WriteBufferToFile() {
-
-// 	now := time.Now().Format(time.RFC3339Nano)
-// 	now = strings.Replace(now, "-", "/", -1)
-// 	now = strings.Replace(now, "T", "/", -1)
-// 	now = strings.Replace(now, ":", "/", 1)
-// 	//	helpers.DebugLog(strings.Split(now, ":")[0])
-// 	err := os.MkdirAll(c.BufferDirectoryPath+strings.Split(now, ":")[0], 0700)
-// 	now = strings.Replace(now, ":", "/", 1)
-// 	helpers.PanicX(err)
-// 	helpers.DebugLog("writing to file:", c.BufferDirectoryPath+now)
-// 	file, err := os.OpenFile(c.BufferDirectoryPath+now, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
-// 	helpers.PanicX(err)
-// 	for i, v := range c.CollectionBuffer {
-// 		// file.Write(v. )
-// 		for ii, iv := range v {
-// 			log.Println("FileName:", iv.Tag)
-// 			// 1. open file by tag/datapoint/timestamp
-// 			// 2. write contents
-
-// 		}
-// 	}
-// 	// buffer.WriteTo(file)
-// }
--- controller/stats.go
+++ controller/stats.go
@@ -2,112 +2,7 @@ package controller
 
 import (
 	"encoding/binary"
-	"log"
 )
-
-type ParsedDataPointValues struct {
-	Values []*ParsedDataPoint
-}
-type ParsedCollection struct {
-	Tag       string
-	DPS       []*ParsedDataPoint
-	BasePoint bool
-}
-type ParsedDataPoint struct {
-	SubIndex int
-	Index    int
-	Tag      string
-	Value    int64
-	Group    string
-}
-
-func getData(headerValue int, data []byte, valuePointer int) (index, size int, value int64) {
-	index, size = findOrderAndSize(headerValue)
-	if size == 1 {
-		// log.Print("BB")
-		return index, 0, 0
-	}
-	binaryValue := data[valuePointer+1 : valuePointer+size]
-	postOrNeg := data[valuePointer]
-	if size == 3 {
-		value := binary.LittleEndian.Uint16(binaryValue)
-		if postOrNeg == 0 {
-			// log.Println("WAS NEGATIVE", -int64(value))
-			return index, size, -int64(value)
-		}
-		return index, size, int64(value)
-
-	} else if size == 5 {
-		value := binary.LittleEndian.Uint32(binaryValue)
-		if postOrNeg == 0 {
-			// log.Println("WAS NEGATIVE", -int64(value))
-			return index, size, -int64(value)
-		}
-		return index, size, int64(value)
-
-	} else if size == 9 {
-		value := binary.LittleEndian.Uint64(binaryValue)
-		if postOrNeg == 0 {
-			// log.Println("WAS NEGATIVE:", -int64(value))
-			return index, size, -int64(value)
-		}
-		return index, size, int64(value)
-
-	} else {
-		log.Println("Whooops!")
-		value = 0
-	}
-	return index, size, int64(value)
-}
-func GetNetworkDataFromSection(MainIndex int, data []byte, previousEndingIndex int) (endIndex int, dpl []*ParsedDataPoint) {
-	if data[previousEndingIndex] == 0 {
-		return previousEndingIndex + 1, nil
-	}
-	numberOfinterfaces := int(data[previousEndingIndex])
-	currentPointer := previousEndingIndex + 1
-	for i := 1; i <= numberOfinterfaces; i++ {
-		currentHeaderLength := int(data[currentPointer])
-		currentPointer = currentPointer + 1
-		currentHeaderPointer := currentPointer
-		currentPointer = currentPointer + currentHeaderLength
-		var iftag string
-		for ii := 1; ii <= currentHeaderLength; ii++ {
-			dp := &ParsedDataPoint{}
-			if ii == 1 {
-				value := string(data[currentPointer : currentPointer+int(data[currentHeaderPointer])])
-				iftag = value
-				currentPointer = currentPointer + int(data[currentHeaderPointer])
-				currentHeaderPointer++
-			} else {
-				index, size, value := getData(int(data[currentHeaderPointer]), data, currentPointer)
-				dp.Tag = iftag
-				dp.Index = MainIndex
-				dp.Value = value
-				dp.SubIndex = index
-				dpl = append(dpl, dp)
-				currentPointer = currentPointer + size
-				currentHeaderPointer++
-			}
-		}
-	}
-	endIndex = currentPointer
-	return
-}
-func GetDataFromSection(MainIndex int, data []byte, previousEndingIndex int) (endIndex int, dpl []*ParsedDataPoint) {
-	if data[previousEndingIndex] == 0 {
-		return previousEndingIndex + 1, nil
-	}
-	currentValueIndex := previousEndingIndex + int(data[previousEndingIndex]) + 1
-	headerLength := int(data[previousEndingIndex])
-
-	for i := 1; i <= headerLength; i++ {
-		index, size, value := getData(int(data[previousEndingIndex+i]), data, currentValueIndex)
-		dpl = append(dpl, &ParsedDataPoint{Index: MainIndex, SubIndex: index, Value: value})
-		currentValueIndex = currentValueIndex + size
-		endIndex = currentValueIndex
-	}
-	return
-}
 
 type DP struct {
 	Index int
@@ -134,7 +29,6 @@ func ParseMinimumDataPoint(data []byte, namespaces map[int]string) DPCollection 
 	// log.Println("NETWORK OUT:", networkOUT)
 
 	currentDataIndex := 0
-	// log.Println(namespaces)
 	DPC := DPCollection{}
 	if _, ok := namespaces[1]; ok {
 		DPC.DPS = append(DPC.DPS, DP{Value: int(data[currentDataIndex]), Index: 1})
@@ -168,7 +62,7 @@ func ParseMinimumDataPoint(data []byte, namespaces map[int]string) DPCollection 
 
 	if _, ok := namespaces[4]; ok {
 		lengthOfNetworkOut := int(data[currentDataIndex])
-		dp := DP{Index: 4}
+		dp := DP{Index: 5}
 		if lengthOfNetworkOut == 2 {
 			dp.Value = int(binary.LittleEndian.Uint16(data[currentDataIndex+1 : currentDataIndex+1+lengthOfNetworkOut]))
 		} else if lengthOfNetworkOut == 4 {
@@ -183,46 +77,4 @@ func ParseMinimumDataPoint(data []byte, namespaces map[int]string) DPCollection 
 	}
 
 	return DPC
-}
-func ParseDataPoint(data []byte, tag string) (dpv *ParsedDataPointValues) {
-	dpv = &ParsedDataPointValues{}
-	// log.Println(data)
-	// log.Print(1)
-	diskEndIndex, DiskValue := GetDataFromSection(1, data, 0)
-	dpv.Values = append(dpv.Values, DiskValue...)
-	// log.Print(2, diskEndIndex)
-	memoryEndIndex, MemoryValue := GetDataFromSection(2, data, diskEndIndex)
-	dpv.Values = append(dpv.Values, MemoryValue...)
-	// log.Print(3, memoryEndIndex)
-	loadEndIndex, LoadValue := GetDataFromSection(3, data, memoryEndIndex)
-	dpv.Values = append(dpv.Values, LoadValue...)
-	// log.Print(4, loadEndIndex)
-	entropyEndIndex, EntropyValue := GetDataFromSection(4, data, loadEndIndex)
-	dpv.Values = append(dpv.Values, EntropyValue...)
-	// log.Print(5, entropyEndIndex)
-	networkEndIndex, NetworkValue := GetNetworkDataFromSection(5, data, entropyEndIndex)
-	dpv.Values = append(dpv.Values, NetworkValue...)
-	// log.Print(6, networkEndIndex)
-	_, CPUVaues := GetDataFromSection(6, data, networkEndIndex)
-	dpv.Values = append(dpv.Values, CPUVaues...)
-	// log.Print(7, cpuEndIndex)
-	// for _, v := range dpv.Values {
-	// 	log.Println(v.Index, v.Value)
-	// }
-	// log.Println(dpv.Values)
-	return
-}
-func findOrderAndSize(data int) (index int, size int) {
-
-	if data < 100 {
-		index = data / 10
-		size = data - (index * 10)
-		// log.Println("data:", data, "less then 100", index, size)
-	} else if data > 100 {
-		index = data / 10
-		size = data - (index * 10)
-		// log.Println("data:", data, "more then 100", index, size)
-	}
-
-	return
 }
